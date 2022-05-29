28ccd06f2c26abda3095450585f9331a657ff14c
panxxhub <panezion@outlook.com>
2022-05-29 14:11:10 +0800

    config

--- i608_moveit_config/launch/i608_fake.launch.py
+++ i608_moveit_config/launch/i608_fake.launch.py
@@ -1,9 +1,5 @@
-import os
-
-import yaml
 from ament_index_python import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import ExecuteProcess
 from launch_ros.actions import Node
 from moveit_configs_utils import MoveItConfigsBuilder
 
@@ -20,4 +16,29 @@ def generate_launch_description():
 
     move_group_node = Node(
         package="moveit_ros_move_group",
+        executable="move_group",
+        output="screen",
+        parameters=[moveit_config.to_dict()]
     )
+
+    static_tf_node = Node(
+        package="tf2_ros",
+        executable="static_transform_publisher",
+        name="static_transform_publisher",
+        output="log",
+        arguments=["0", "0", "0", "0", "0", "0", "world", "base_link"],
+    )
+
+    robot_state_publisher = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        name="robot_state_publisher",
+        output="log",
+        parameters=[moveit_config.robot_description],
+    )
+
+    return LaunchDescription([
+        static_tf_node,
+        robot_state_publisher,
+        move_group_node,
+    ])
